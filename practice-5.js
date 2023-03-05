1eb5b6f19a8734c9ebf486bdc76ec63008dcf47d
Sergey Kuran <kuran.serg@gmail.com>
2023-03-05 17:16:21 +0200

    Create practice-5.js

---
+++ practice-5.js
@@ -0,0 +1,383 @@
+// // // Виконай рефакторинг методів об'єкта pizzaPalace, розставивши відсутні this в місцях звернення до властивостей і методів об'єкта.
+
+// // // Метод checkPizza об'єкта pizzaPalace використовує this.
+// // // Метод order об'єкта pizzaPalace використовує this
+// // // Виклик pizzaPalace.order("Smoked") повертає рядок "Order accepted, preparing «Smoked» pizza"
+// // // Виклик pizzaPalace.order("Four meats") повертає рядок "Order accepted, preparing «Four meats» pizza"
+// // // Виклик pizzaPalace.order("Big Mike") повертає рядок "Sorry, there is no pizza named «Big Mike»"
+// // // Виклик pizzaPalace.order("Viennese") повертає рядок "Sorry, there is no pizza named «Viennese»"
+
+// // const pizzaPalace = {
+// //   pizzas: ["Supercheese", "Smoked", "Four meats"],
+// //   // Change code below this line
+// //   checkPizza(pizzaName) {
+// //     return this.pizzas.includes(pizzaName);
+// //   },
+// //   order(pizzaName) {
+// //     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+// //     if (!isPizzaAvailable) {
+// //       return `Sorry, there is no pizza named «${pizzaName}»`;
+// //     }
+
+// //     return `Order accepted, preparing «${pizzaName}» pizza`;
+// //   },
+// //   // Change code above this line
+// // };
+
+
+// Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
+
+// Після оголошення об'єкта ми додали виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
+
+// Оголошена змінна customer
+// Значення змінної customer - це об'єкт з властивостями і методами
+// Виклик customer.getDiscount() повертає поточне значення властивості discount
+// Виклик customer.setDiscount(0.15) оновлює значення властивості discount
+// Виклик customer.getBalance() повертає поточне значення властивості balance.
+// Виклик customer.getOrders() повертає поточне значення властивості orders
+// Виклик customer.addOrder(5000, "Steak") додає "Steak" в масив значень властивості orders і оновлює баланс
+// Метод getBalance об'єкта customer використовує this
+// Метод getDiscount об'єкта customer використовує this
+// Метод setDiscount об'єкта customer використовує this
+// Метод getOrders об'єкта customer використовує this
+// Метод addOrder об'єкта customer використовує this
+
+
+// const customer = {
+//   username: "Mango",
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ["Burger", "Pizza", "Salad"],
+//   // Change code below this line
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Change code above this line
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, "Steak");
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+
+// Тестувальники знайшли баги в коді сервісу зберігання історії замовлень їжі. Тобі необхідно виправити їх, правильно розставивши this в методах об'єкта historyService, щоб методи почали працювати правильно.
+
+// Оголошена змінна historyService
+// Значення змінної historyService - це об'єкт з вихідними властивостями та методами
+// Виклик historyService.getOrdersLog() повертає рядок з переліком даних всіх замовлень з властивості orders
+// Виклик historyService.getEmails() повертає масив всіх унікальних поштових адрес з властивості orders
+// Виклик historyService.getOrdersByEmail("solomon@topmail.net") повертає [{ email: "solomon@topmail.net", dish: "Burger" }, { email: "solomon@topmail.net", dish: "Apple pie" }]
+// Виклик historyService.getOrdersByEmail("artemis@coldmail.net") повертає [{ email: "artemis@coldmail.net", dish: "Pizza" }]
+// Метод getOrdersLog об'єкта historyService використовує this
+// Метод getEmails об'єкта historyService використовує this
+// Метод getOrdersByEmail об'єкта historyService використовує this
+
+
+// const historyService = {
+//   orders: [
+//     { email: "jacob@hotmail.com", dish: "Burrito" },
+//     { email: "solomon@topmail.net", dish: "Burger" },
+//     { email: "artemis@coldmail.net", dish: "Pizza" },
+//     { email: "solomon@topmail.net", dish: "Apple pie" },
+//     { email: "jacob@hotmail.com", dish: "Taco" },
+//   ],
+
+//   getOrdersLog() {
+//     return this.orders.map(order => `email: ${order.email} dish: ${order.dish}`).join(" - ");
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(order => order.email === email);
+//   },
+
+// };
+
+
+// Зміни код таким чином, щоб об'єкт parent став прототипом для об'єкта у змінній сhild.
+
+// Оголошена змінна parent
+// Значення змінної parent - це об'єкт
+// Виклик parent.hasOwnProperty("surname") повертає true
+// Виклик parent.hasOwnProperty("heritage") повертає true
+// Оголошена змінна child
+// Значення змінної child - це об'єкт
+// Виклик child.hasOwnProperty("name") повертає true
+// Звернення до child.name повертає "Jason"
+// Виклик child.hasOwnProperty("age") повертає true
+// Звернення до child.age повертає 27
+// Виклик child.hasOwnProperty("surname") повертає false
+// Звернення до child.surname повертає "Moore"
+// Виклик child.hasOwnProperty("heritage") повертає false
+// Звернення до child.heritage повертає "Irish"
+// Виклик parent.isPrototypeOf(child) повертає true
+// Використовується метод Object.create()
+
+
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Change code below this line
+
+// const child = Object.create(parent);
+
+// // Change code above this line
+// child.name = "Jason";
+// child.age = 27;
+
+
+
+// Зміни код, побудувавши ланцюжок прототипів таким чином, щоб об'єкт ancestor був прототипом для parent, а той, своєю чергою, був прототипом для child.
+
+// Оголошена змінна ancestor
+// Значення змінної ancestor - це об'єкт.
+// Оголошена змінна parent
+// Значення змінної parent - це об'єкт.
+// Оголошена змінна child
+// Значення змінної child - це об'єкт.
+// Виклик ancestor.isPrototypeOf("parent") повертає true
+// Виклик parent.isPrototypeOf("child") повертає true
+// Виклик ancestor.hasOwnProperty("surname") повертає true
+// Звернення до ancestor.surname повертає "Dawson"
+// Виклик parent.hasOwnProperty("surname") повертає true
+// Звернення до parent.surname повертає "Moore"
+// Виклик child.hasOwnProperty("surname") повертає false
+// Звернення до child.surname повертає "Moore"
+// Виклик ancestor.hasOwnProperty("heritage") повертає true
+// Звернення до ancestor.heritage повертає "Irish"
+// Виклик parent.hasOwnProperty("heritage") повертає false
+// Звернення до parent.heritage повертає "Irish"
+// Виклик child.hasOwnProperty("heritage") повертає false
+// Звернення до child.heritage повертає "Irish"
+// Використовується метод Object.create()
+
+
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
+
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// // Change code above this line
+
+
+// Використовуючи ключове слово class, оголоси клас Car з порожнім тілом.
+
+// Оголошений клас Car
+// Результат виклику new Car() - це порожній об'єкт
+
+// class Car{}
+
+
+// Додай класу Car метод constructor, який приймає три параметри:
+
+// brand - марка автомобіля.
+// model - модель автомобіля.
+// price - ціна автомобіля.
+// Клас Car повинен створювати об'єкт з однойменними властивостями brand, model і price, значеннями яких повинні бути передані аргументи під час його виклику з оператором new.
+
+// Оголошений клас Car
+// Клас Car має метод constructor
+// В результаті виклику new Car("Audi", "Q3", 36000) утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
+// В результаті виклику new Car("BMW", "X5", 58900) утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
+// В результаті виклику new Car("Nissan","Murano", 31700) утвориться об'єкт { brand: "Nissan", model: "Murano", price: 31700 }
+
+
+// class Car {
+//   constructor(brand, model, price){
+//   this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// };
+
+// console.log(new Car("Audi", "Q3", 36000));
+
+
+// Виконай рефакторинг класу Car таким чином, щоб він приймав один параметр - об'єкт з властивостями brand, model і price. Деструктуризуй об'єкт в сигнатурі (підписі) конструктора.
+
+// Оголошений клас Car
+// Клас Car має метод constructor
+// В результаті виклику new Car({ brand: "Audi", model: "Q3", price: 36000 }) утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
+// В результаті виклику new Car({ brand: "BMW", model: "X5", price: 58900 }) утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
+// В результаті виклику new Car({ brand: "Nissan", model: "Murano", price: 31700 }) утвориться об'єкт { brand: "Nissan", model: "Murano", price: 31700 }
+
+
+// class Car {
+  
+//   constructor({brand, model, price}) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+
+// };
+// console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 }));
+
+
+
+// Додай класу Car два методи.
+
+// getPrice() - повертає значення властивості price з об'єкта, який буде його викликати.
+// changePrice(newPrice) - оновлює значення властивості price в об'єкта, який буде його викликати на newPrice.
+// В класі Car оголошений метод getPrice
+// Метод getPrice повертає значення властивості price екземпляра класу, який його викликає
+// В класі Car оголошений метод changePrice
+// Метод changePrice змінює значення властивості price екземпляра класу, який його викликає
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+// getPrice(){
+//   return this.price;
+// }
+  
+// changePrice(newPrice){
+// return this.price = newPrice;
+// }
+
+//   // Change code above this line
+// }
+
+
+// Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. Клас очікує тільки один аргумент - початковий масив товарів, який записується у властивість items об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getItems() - повертає масив поточних товарів у властивості items об'єкта, який викликає цей метод.
+// addItem(newItem) - приймає новий товар newItem і додає його в масив товарів у властивості items об'єкта, який викликає цей метод.
+// removeItem(itemToRemove) - приймає товар itemToRemove і видаляє його з масиву товарів у властивості items об'єкта, який викликає цей метод.
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
+
+// Оголошений клас Storage
+// В класі Storage оголошений метод getItems
+// В класі Storage оголошений метод addItem
+// В класі Storage оголошений метод removeItem
+// Метод getItems повертає значення властивості items екземпляра класу, який його викликає
+// Метод addItem змінює властивість items екземпляра класу, який його викликає
+// Метод removeItem змінює властивість items екземпляра класу, який його викликає
+// В результаті виклику new Storage(["Nanitoids", "Prolonger", "Antigravitator"]) значення змінної storage - це об'єкт
+// Об'єкт storage містить властивість items
+// Перший виклик storage.getItems(), відразу після інціалізаціі екземпляра, повертає масив ["Nanitoids", "Prolonger", "Antigravitator"]
+// Другий виклик, storage.getItems(), після виклику storage.addItem("Droid"), повертає масив ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// Третій виклик storage.getItems(), після виклику storage.removeItem("Prolonger"), повертає масив ["Nanitoids", "Antigravitator", "Droid"]
+
+
+// class Storage{
+//    constructor(items) {
+//     this.items = items;
+//   }
+//   getItems(){
+//     return this.items;
+//   }
+//   addItem(newItem){
+//     this.items.push(newItem);
+//   }
+//     removeItem(itemToRemove) {
+      
+//       this.items.splice(this.items.indexOf(itemToRemove),1);
+
+//   }
+// };
+
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+
+// Напиши клас StringBuilder, який приймає один параметр initialValue - довільний рядок, який записується у властивість value об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+
+// getValue() - повертає поточне значення властивості value.
+// padEnd(str) - отримує параметр str (рядок) і додає його в кінець значення властивості value об'єкта, який викликає цей метод.
+// padStart(str) - отримує параметр str (рядок) і додає його на початок значення властивості value об'єкта, який викликає цей метод.
+// padBoth(str) - отримує параметр str (рядок) і додає його на початок і в кінець значення властивості value об'єкта, який викликає цей метод.
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
+
+// Оголошений клас StringBuilder
+// В класі StringBuilder оголошений метод getValue
+// Метод getValue повертає значення властивості value екземпляра класу, який його викликає
+// В класі StringBuilder оголошений метод padEnd
+// Метод padEnd змінює властивість value екземпляра класу, який його викликає
+// В класі StringBuilder оголошений метод padStart
+// Метод padStart змінює властивість value екземпляра класу, який його викликає
+// В класі StringBuilder оголошений метод padBoth
+// Метод padBoth змінює властивість value екземпляра класу, який його викликає
+// В результаті виклику new StringBuilder(".") значення змінної builder - це об'єкт
+// Об'єкт builder містить властивість value
+// Перший виклик builder.getValue(), відразу після ініціалізації екземпляра, повертає рядок .
+// Другий виклик builder.getValue(), після виклику builder.padStart("^"), повертає рядок ^.
+// Третій виклик builder.getValue(), після виклику builder.padEnd("^"), повертає рядок ^.^
+// Четвертий виклик builder.getValue(), після виклику builder.padBoth("="), повертає рядок =^.^=
+
+
+class StringBuilder{
+  constructor(initialValue){
+    this.initialValue;
+  }
+  getValue(){
+    this.value;
+  }
+  // padEnd(str){
+  //   this.value.push(str);
+  // }
+  // padStart(str){
+  //   this.value.unshift(str);
+  // }
+  // padBoth(str){
+  //   this.value.push();
+  //   this.value.unshift();
+  // }
+}
+
+
+// Change code above this line
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
